14cfe2313d85940c9ee7c243e71cf45ee7254e80
ryanhomrh <91391395+ryanhomrh@users.noreply.github.com>
2025-12-24 15:20:45 -0800

    build function to get list of video ids

--- video_stats.py
+++ video_stats.py
@@ -9,6 +9,7 @@ load_dotenv(dotenv_path="./.env")
 
 API_KEY = os.getenv("API_KEY")
 CHANNEL_HANDLE = "MrBeast"
+maxResults = 50
 
 def get_playlist_id():
 
@@ -19,7 +20,6 @@ def get_playlist_id():
         response = requests.get(url)
 
         response.raise_for_status()
-
 
         data = response.json()
 
@@ -44,6 +44,48 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         raise e
 
+
+def get_video_ids(playlistId):
+
+    video_ids = []
+
+    pageToken = None
+
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=ContentDetails&maxResults={maxResults}&playlistId={playlistId}&key={API_KEY}"
+
+## For the first run, since we don't have a pagetoken we skip line 68 of the 
+    try:
+
+        while True:
+            url = base_url
+
+            if pageToken: ##if pageToken has a not None value type (aka it exists) then append this to the url else break the while loop
+                url += f"&pageToken={pageToken}"
+
+            response = requests.get(url)
+
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get('items', []):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+            
+            pageToken = data.get('nextPageToken')
+
+            if not pageToken:
+                break
+
+        return video_ids
+
+    
+    except requests.exceptions.RequestException as e:
+        raise e
+
+
+
 ##__name__ is a special built in variable. When this file is run directly, __name__ is called __main__. This bottom check allows us to only run this script when it is called directly and not imported as a module.
 if __name__ == "__main__":
-    get_playlist_id()
+    playlistId = get_playlist_id()
+    get_video_ids(playlistId)
